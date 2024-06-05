0a329c9be9826f55e5a1bcc3336cc6bddf4c1b17
Sophie Baumann <153180755+s-bau@users.noreply.github.com>
2024-06-05 13:31:45 +0200

    Delete logistique_1.sql

--- logistique_1.sql
+++
@@ -1,7 +0,0 @@
-/* 5 meilleures ventes et stock */
-SELECT od.productCode, p.productName AS productName, SUM(quantityOrdered) AS totalSale, quantityInStock
-FROM orderdetails AS od
-JOIN products AS p ON p.productCode = od.productCode
-GROUP BY od.productCode
-ORDER BY totalSale DESC
-LIMIT 5;
