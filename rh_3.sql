b881f1b7b0ea337ff25b3a406ba83aacdf76183a
Sophie Baumann <153180755+s-bau@users.noreply.github.com>
2024-06-05 13:29:59 +0200

    Delete rh_3.sql

--- rh_3.sql
+++
@@ -1,5 +0,0 @@
-SELECT CONCAT(employees.firstName," ",employees.lastName) AS full_name, CONCAT(offices.city, ", ", offices.country) AS bureau, customers.customerName, CONCAT(customers.city, ", ", customers.country) AS client_location, payments.paymentDate, payments.amount
-FROM employees
-JOIN customers ON employees.employeeNumber = customers.salesRepEmployeeNumber
-JOIN payments ON customers.customerNumber = payments.customerNumber
-JOIN offices ON offices.officeCode = employees.officeCode;
